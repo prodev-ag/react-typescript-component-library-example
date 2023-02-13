6d66ed81a24200181d7697b58421427a0999075f
prodev-ag <prodev.ag@gmail.com>
2023-02-13 05:35:25 +0400

    feat: add husky && lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  '*.md': ['prettier --write --parser markdown'],
+  '*.yaml': ['prettier --write --parser yaml'],
+  '*.json': ['prettier --write --parser json'],
+  '*.{js,ts,tsx}': ['prettier --write', 'eslint --fix'],
+};
